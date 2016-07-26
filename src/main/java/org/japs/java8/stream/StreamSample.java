6e13a25636c646e2bb41f25be14fe1d3a21a6f63
hatimiti <hatti33@gmail.com>
2016-07-27 01:25:35 +0900

    Updates text.

--- src/main/java/org/japs/java8/stream/StreamSample.java
+++ src/main/java/org/japs/java8/stream/StreamSample.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamSample {
@@ -26,16 +30,11 @@ public class StreamSample {
 		checkSequentialFromParallelStream();
 		checkFlatMap();
 		checkReduce(persons);
-
-		long a = Stream.of("a", "b", "c").count();
-		System.out.println(a);
 		
-		List<String> o = Arrays.asList("a", "b");
-		List<String> l = new ArrayList<>(o);
-		l.replaceAll(String::toUpperCase);
-		System.out.println(o);
-		System.out.println(l);
-
+		createStream();
+		terminalOperation();
+		
+		System.out.println(new Random(System.currentTimeMillis()).nextInt(9));
 	}
 
 	private static void checkReduce(List<Person> persons) {
@@ -77,16 +76,14 @@ public class StreamSample {
 		);
 	}
 
-	private static void averageOfAge(List<Person> persons) {
-		System.out.println(
-			persons.stream()
-				.filter(p -> p.getAge() >= 30) 
-				.mapToInt(p -> p.getSalary())
-				.summaryStatistics().getAverage()
-		);
+	private static double averageOfAge(List<Person> persons) {
+		return persons.stream()
+			.filter(p -> p.getAge() >= 30) 
+			.mapToInt(p -> p.getSalary())
+			.summaryStatistics().getAverage();
 	}
 	
-	private static void averageOfAgeByLoop(List<Person> persons) {
+	private static double averageOfAgeByLoop(List<Person> persons) {
 		double result = 0.0;
 		int sum = 0;
 		int count = 0;
@@ -99,9 +96,61 @@ public class StreamSample {
 		if (count != 0) {
 			result = ((double) sum) / count;
 		}
-		System.out.println(result);
+		return result;
+	}
+	
+	@SuppressWarnings("unused")
+	private static void createStream() {
+		Stream<String> s1  = Stream.of("Hello Stream");
+		Stream<String> s2  = Stream.of("Hello", "Stream");
+		Stream<String> s3  = Stream.empty();
+		IntStream      s4  = IntStream.iterate(1, n -> n + n); //1,2,4,8,16,...
+		DoubleStream   s5  = DoubleStream.generate(() -> Math.random()); // 乱数の無限Stream
+		IntStream      s6  = IntStream.range(1, 10); // 1〜9
+		IntStream      s7  = IntStream.rangeClosed(1, 10); // 1〜10
+		Stream<String> s8  = Arrays.asList("a", "b")/*List<String>*/.stream();
+		Stream<String> s9  = Arrays.asList("a", "b")/*List<String>*/.parallelStream();
+		Stream<String> s10 = Arrays.stream(new String[] { "Hello", "Stream"});
 	}
 
+	private static void terminalOperation() {
+		/*#1*/
+		Stream.of("a", "b", "c").forEach(e -> System.out.print(e)); //abc
+		System.out.println();
+		/*#2*/
+		Stream.of("b", "c", "a").parallel().forEachOrdered(e -> System.out.print(e)); //bca
+		System.out.println();
+		/*#3*/
+		Optional<Integer> x = Stream.of(3, 1, 2).min((i, j) -> i.compareTo(j));
+		System.out.println(x); // Optional[1]
+		/*#4*/
+		Optional<Integer> y = Stream.of(3, 1, 2).max((i, j) -> i.compareTo(j));
+		System.out.println(y); // Optional[3]
+		/*#5*/
+		long z = Stream.of(0, 0, 0).count();
+		System.out.println(z); // 3
+		/*#6*/
+		boolean a = Stream.of(1, 2, 3).anyMatch(e -> e.equals(2));
+		System.out.println(a); // true
+		/*#7*/
+		boolean b = Stream.of(2, 2, 2).allMatch(e -> e.equals(2));
+		System.out.println(b); // true
+		/*#8*/
+		boolean c = Stream.of(2, 2, 2).noneMatch(e -> e.equals(3));
+		System.out.println(c); // true
+		/*#9*/
+		Optional<Integer> d = Stream.of(2, 1, 3).findFirst();
+		System.out.println(d); // Optional[2]
+		/*#10*/
+		Optional<Integer> e = Stream.of(2, 1, 3).findAny();
+		System.out.println(e); // Optional[2]
+		/*#11*/
+		Object[] f = Stream.of(2, 1, 3).toArray();
+		System.out.println(f); // [Ljava.lang.Object;@17f052a3 
+		/*#12*/
+		Integer[] g = Stream.of(2, 1, 3).toArray(i -> new Integer[i]);
+		System.out.println(g); // [Ljava.lang.Integer;@685f4c2e
+	}
 }
 
 class Person {
